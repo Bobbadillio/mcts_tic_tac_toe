f581eeaf7f1efa69dca8a7c877016b65e56fa166
festivity <nrk139@gmail.com>
2018-08-07 22:53:17 -0500

    adding code to allow mcts player to play forcing moves when they're known, to only include active player forced wins as children

--- tictac_mcts.py
+++ tictac_mcts.py
@@ -50,36 +50,65 @@ class MCTSNode():
         return [child[0] for child in self.children]
 
     def extend(self):
-        if self.board.is_final():
+        if self.n_visits == 0:
+            self.n_visits += 1
+            # create child boards on first visit
+            # notably, this assumes deterministic transition between states
+            for move in self.board.get_available_moves():
+                # consider each move
+                # TODO: stop considering new moves if this is a forced win
+                new_board = copy.deepcopy(self.board)
+                new_board.enter_move(move)
+                new_node = MCTSNode(not self.should_maximize, new_board)
+                if str(new_board) in terminal_board_dict:
+                    new_result = terminal_board_dict[str(new_board)]
+                    if self.should_maximize and new_result == 'x':
+                        terminal_board_dict[str(self.board)] = 'x'
+                        self.children = [(new_node,move)]
+                        break
+                    elif not self.should_maximize and new_result == 'o':
+                        terminal_board_dict[str(self.board)] = 'o'
+                        self.children = [(new_node,move)]
+                        break
+                    else:
+                        self.children.append((new_node,move))
+                else:
+                    self.children.append((new_node,move))
+
+            if str(self.board) in terminal_board_dict:
+                result = terminal_board_dict[str(self.board)]
+            else:
+                result = raw_eval(self.board)
+
+            # process result of playout
+            result_points = Board.point_translator(result)
+            self.points = result_points
+            return result_points
+        elif str(self.board) in terminal_board_dict:
+            winning_token = terminal_board_dict[str(self.board)]
+            self.n_visits += 1
+            terminal_points = Board.point_translator(winning_token)
+            self.n_points = terminal_points
+
+            return self.n_points
+
+        elif self.board.is_final():
             raw_eval(self.board)
             self.n_visits += 1
             self.n_points = self.board.get_points()
             return self.board.get_points()
-        if self.n_visits == 0:
-            # created child boards on first visit
-            # notably, this assumes deterministic transition between states
-            for move in self.board.get_available_moves():
-#                print( self.board.get_available_moves())
-                new_board = copy.deepcopy(self.board)
-                new_board.enter_move(move)
-                new_node = MCTSNode(not self.should_maximize, new_board)
-                self.children.append((new_node,move))
-
-            result = raw_eval(self.board)
-
-            # process result of playout
-            if result == 'cat':
-                result_points = .5
-            if result == 'x':
-                result_points = 1
-            if result == 'o':
-                result_points = 0
-            self.n_visits = 1
-            self.points = result_points
-            return result_points
 
         elif (not self.is_fully_expanded):
+            self.n_visits += 1
             # create a filter for children that have a forced win for me
+            winning_children = filter(
+                lambda a_child: (
+                    str(a_child[0].board) in terminal_board_dict
+                    ) and (
+                        terminal_board_dict == 'x' if self.should_maximize else 'o'),
+                self.get_child_nodes()
+            )
+
             # move to that child and add this node to the list of forced
             # modify to filter for children that aren't forced for opponent
             unexplored_children = filter(
@@ -122,15 +151,26 @@ class MCTSPlayer(Player):
             should_maximize=self.token == 'x',
             board = copy.deepcopy(board)
         )
-        for i in range(400):
+        n_rollouts = 400
+        for i in range(n_rollouts):
+            solved_child = False
+            for child in mcts_master.children:
+                if ((str(child[0].board) in terminal_board_dict)
+                and (terminal_board_dict[str(child[0].board)] == self.token)):
+                    print("early exit on board:\n{}\nmove {}".format(
+                        board,
+                        child[1]
+                        )
+                    )
+                    return child[1]
             mcts_master.extend()
         most_visited = max(
             mcts_master.children,
             key = lambda a_child: a_child[0].n_visits
         )
+        #for key in terminal_board_dict.keys():
+        #    print("{}\n{}\n===========".format(key,terminal_board_dict[key]))
         return most_visited[1]
-#        available = board.get_available_moves()
-#        return random.choice(available)
 
 
 if __name__ == '__main__':
